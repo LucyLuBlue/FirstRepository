27f556cc0690194da367ee6855f0d647d38833bc
Lucy Lagunas <lucylu858@gmail.com>
2019-04-25 13:22:09 +0100

    Third Commit

--- src/lucygit/main.java
+++ src/lucygit/main.java
@@ -5,12 +5,6 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is change one");
-System.out.println("this is change one");
-System.out.println("this is change one");
-System.out.println("this is change one");
-System.out.println("this is change one");
-System.out.println("this is change one");
-System.out.println("this is change one");
 
 	}
 
